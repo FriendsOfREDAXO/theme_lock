cffcc371ee49b0eaf17e494ebe347963f1b72712
Thomas Skerbis <thomas.skerbis@mac.com>
2022-01-23 15:17:52 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -2,6 +2,8 @@
 if (rex::isBackend())
 { 
    rex_extension::register('PACKAGES_INCLUDED', static function (rex_extension_point $ep) {
-    rex::setProperty('theme', 'light');
+   rex::setProperty('theme', 'light');
+   // CKE5 fix   
+   rex_view::setJsProperty('cke5theme', 'light');   
    }, rex_extension::EARLY);  
 }
